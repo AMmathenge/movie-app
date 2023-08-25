46ddfe8c3b99e8b1ebaa9ecd4cc36c8017c420b9
AMmathenge <anthonymathenge191@gmail.com>
2023-08-25 13:28:05 +0300

    search functionality

--- src/lib/components/searchMovies.svelte
+++ src/lib/components/searchMovies.svelte
@@ -1,7 +1,45 @@
-<form action="">
-    <label for="">
-        Search Movies
-    </label>
-    <input type="text" name="" id="">
-    <button>search</button>
+<script>
+    import global from '../../global.css'
+    import {goto} from '$app/navigation'
+    import {onMount} from 'svelte';
+    import { fly } from "svelte/animate"
+
+    let inputValue = '';
+    let active = false;
+
+
+
+    function cancelInactive() {
+        if(inputValue){
+            active = true;
+        }else{
+            active =false;
+        }
+    }
+    function submitSearch(){
+        goto( '/search/'+ inputValue);
+    }
+    
+</script>
+<form on:submit|preventDefault={submitSearch} class="relative">
+
+    <div   class="  ">
+       
+        
+        <div   class="gap-4 w-1/4  ">
+            {#if !active }
+            <label in:fly={{y: -10, duration: 500}} out:fly={{y: -10, duration: 500}} class=" absolute text-green-500 px-4 py-2" >
+                Search Movies
+            </label>
+            {/if}
+            <input in:fly={{y: -10, duration: 500}} out:fly={{y: -10, duration: 500}} on:blur={cancelInactive}  on:focus={() => (active = true)} bind:value={inputValue} class="h-full w-full text-white  px-4 py-2  w-full rounded-2xl outline-none  bg-lime-950" type="text" name="" id="">
+            {#if inputValue}
+                <button type="submit" class="absolute h-full top-0 left-64 bg-blue-700  px-4 py-2 rounded-r-xl text-white cursor-white font-bold h-full">
+                    search
+                </button>
+            {/if}
+            
+        </div>
+    </div>
+
 </form>
---
+++ src/routes/search/[id]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import  {MovieCard} from '$lib/components';
+    export let data;
+    
+    console.log(data)
+</script>
+<div class="popular-movies grid grid-cols-auto-fit sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 minmax(200px, 1fr) gap-x-8 gap-y-4">
+{#each data.props.data as searchedMovies  }
+   <MovieCard movie={searchedMovies}/> 
+{/each}
+</div>
